c9191dfb9cabe4002af34630e49d89873a834547
Miguel Barro <miguelbarro@eprosima.com>
2021-05-10 11:32:58 +0200

    [11489] update package scripts for VS2019
    Signed-off-by: Miguel Barro <miguelbarro@eprosima.com>

--- foonathan_memory-config.cmake
+++ foonathan_memory-config.cmake
@@ -20,13 +20,21 @@ if(MSVC_VERSION EQUAL 1900)
         include(
           "${CMAKE_CURRENT_LIST_DIR}/../share/foonathan_memory-i86Win32VS2015/cmake/foonathan_memory-config.cmake")
     endif()
-elseif(MSVC_VERSION GREATER 1900)
+elseif(MSVC_VERSION LESS 1920)
     if(CMAKE_CL_64)
         include(
           "${CMAKE_CURRENT_LIST_DIR}/../share/foonathan_memory-x64Win64VS2017/cmake/foonathan_memory-config.cmake")
     else()
         include(
           "${CMAKE_CURRENT_LIST_DIR}/../share/foonathan_memory-i86Win32VS2017/cmake/foonathan_memory-config.cmake")
+    endif()
+elseif(MSVC_VERSION GREATER_EQUAL 1920)
+    if(CMAKE_CL_64)
+        include(
+          "${CMAKE_CURRENT_LIST_DIR}/../share/foonathan_memory-x64Win64VS2019/cmake/foonathan_memory-config.cmake")
+    else()
+        include(
+          "${CMAKE_CURRENT_LIST_DIR}/../share/foonathan_memory-i86Win32VS2019/cmake/foonathan_memory-config.cmake")
     endif()
 else()
     message(FATAL_ERROR "Not supported version of Visual Studio")
